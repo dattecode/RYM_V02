537c1779113e0cb481cc32ab7fc3d302cdaa3d33
dattecode <dattecode@gmail.com>
2023-10-15 18:09:48 -0300

    solution_visual_bugs

--- src/components/Alone.jsx
+++ src/components/Alone.jsx
@@ -8,7 +8,7 @@ const Alone = () => {
 
     <div className='alone'>
       <img src={imgBg} className='aloneImg' />
-      <h2 className='infoAlone'>This dimension is empty</h2>
+      <h6 className='infoAlone'>This dimension is empty</h6>
     </div>
   )
 }
